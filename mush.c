d0be4e3f21f34ef1054aa5a0774da1e85fe19615
Daniel Smith <dsmith74@calpoly.edu>
2018-03-14 03:21:19 -0700

    Fixed for cl vs file

--- mush.c
+++ mush.c
@@ -3,7 +3,35 @@
 
 static int c_terminate;
 
-int execute_commands () {
+int execute_file () {
+    struct sigaction sa;
+
+    c_terminate = 0;
+
+    sa.sa_handler = &handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+    while (/*end of file isn't yet reached*/) {
+        /* parse the command line */
+        parseline();
+ 
+        /* execute the command */
+                
+
+        /* check for termination */
+        if (c_terminate == 1) {
+            printf("HAHAHA\n");
+            break;
+            /* change this to waiting for all children to die */
+        }
+    }
+    return 0;
+}
+ 
+
+int execute_command_line () {
     struct sigaction sa;
 
     c_terminate = 0;
@@ -19,6 +47,9 @@ int execute_commands () {
         parseline();
  
         /* execute the command */
+                
+
+        /* check for termination */
         if (c_terminate == 1) {
             printf("HAHAHA\n");
             break;
